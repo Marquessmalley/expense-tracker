80ec91f4d7f9f0e2a5353d79f705f3a10d55eec9
Marques Smalley <marquessmalley@gmail.com>
2022-04-26 15:15:56 -0400

    add expense, expenseItem and expenseDate component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Expenses from "./components/Expenses";
 function App() {
   return (
     <div className="App">
       <h1>Expense Tracker App</h1>
+      <Expenses />
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ExpenseDate.css";
+const ExpenseDate = ({ date }) => {
+  const month = date.toLocaleString("en-us", { month: "long" });
+  const year = date.toLocaleString("en-us", { month: "long" });
+  const day = date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
