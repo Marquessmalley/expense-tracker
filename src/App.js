1f6b1636c75af0cf67f31a1e81d926f13353c0b5
Marques Smalley <marquessmalley@gmail.com>
2022-04-27 09:16:29 -0400

    update to arrow functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Expenses from "./components/Expenses/Expenses";
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Expense Tracker App</h1>
       <Expenses />
     </div>
   );
-}
+};
 
 export default App;
